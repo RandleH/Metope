50eccef1e6d3a77e999d3bb25d2282c26c386a81
RandleH's MacbookAirM1 <randle.helmslay@gmail.com>
2024-10-29 11:46:26 -0700

    Add autogen-backup

--- rebuild.sh
+++ rebuild.sh
@@ -12,3 +12,6 @@ cd ./build
 cmake -DCMAKE_BUILD_TYPE=Debug -DCHIP=STM32F411CEU6 -DTEST_ONLY=1 ..
 make -j32
 
+# Backup Auto-Generated Files
+cp -rf ${DIR_CWD}/STM32CubeMX/STM32F405RGT6 ${DIR_CWD}/backup/STM32F405RGT6
+cp -rf ${DIR_CWD}/STM32CubeMX/STM32F411CEU6 ${DIR_CWD}/backup/STM32F411CEU6
