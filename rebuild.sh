4ecf5fb8652744a62d640858d477854ec6a9737f
RandleH's MacbookAirM1 <randle.helmslay@gmail.com>
2024-12-24 18:11:04 -0800

    Remove local dependency nm/size/objdump

--- rebuild.sh
+++ rebuild.sh
@@ -22,7 +22,6 @@ cmake -DCMAKE_BUILD_TYPE=Debug -DCHIP=STM32F411CEU6 -DUSE_REGISTER=1 ..
 # cmake -DCMAKE_BUILD_TYPE=Debug -DCHIP=STM32F405RGT6 -DTEST_ONLY=1 -DUSE_REGISTER=1 -DINCLUDE_TB_OS=1  ..
 
 make -j32
-arm-none-eabi-size ${DIR_CWD}/build/model1.elf
 
 # Backup Auto-Generated Files
 # cp -rf ${DIR_CWD}/STM32CubeMX/STM32F405RGT6 ${DIR_CWD}/backup/STM32F405RGT6
